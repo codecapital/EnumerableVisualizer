24a6fc9a7ee7b22e0c994d352c16aef8c4457c97
Vaclav Elias <VaclavElias@gmail.com>
2019-06-11 14:17:04 +0100

    Class separated to individual files in Test

---
+++ EnumerableVisualizer.Tests/Gadget.cs
@@ -0,0 +1,8 @@
+﻿namespace EnumerableVisualizer.Tests
+{
+    public class Gadget
+    {
+        public string Name { get; set; }
+        public Gadget(string name) => Name = name;
+    }
+}
---
+++ EnumerableVisualizer.Tests/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace EnumerableVisualizer.Tests
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Car Car { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age, Car car)
+        {
+            Name = name;
+            Age = age;
+            Car = car;
+        }
+    }
+}
