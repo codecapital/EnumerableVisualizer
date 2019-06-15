227d4043410f74174308fdbfede7513c43ec3d77
Vaclav Elias <VaclavElias@gmail.com>
2019-06-15 17:54:02 +0100

    Common classes separated

---
+++ EnumerableVisualizer.SharedTests/Gadget.cs
@@ -0,0 +1,8 @@
+﻿namespace EnumerableVisualizer.SharedTests
+{
+    public class Gadget
+    {
+        public string Name { get; set; }
+        public Gadget(string name) => Name = name;
+    }
+}
